bbf6f907ac1d30aa578c6a4a99ea85ed13997147
Kaloyan Petrov <kaloyan.petrov7@gmail.com>
2020-12-01 16:44:14 +0200

    Add AoC day 1 / part 2 naive solution

--- day1/main.py
+++ day1/main.py
@@ -1,6 +1,8 @@
-with open("./input.txt") as file:
-    data = file.readlines()
+import functools
+import random
 
+
+def first_part(data):
     current = None
     used_lines = []
     not_found = True
@@ -15,7 +17,26 @@ with open("./input.txt") as file:
             elif current is not None:
                 res = current + int_line
                 if res == 2020:
-                    print(res)
-                    print(current * int_line)
+                    print("First part:", current * int_line)
                     not_found = False
                     break
+
+
+def second_part(data):
+    items = [0, 0, 0]
+
+    while sum_of_items(items) != 2020:
+        items = [random.choice(data), random.choice(data), random.choice(data)]
+    print("Second part:", functools.reduce(lambda a, b: int(a) * int(b), items))
+
+
+def sum_of_items(items):
+    return functools.reduce(lambda a, b: int(a) + int(b), items)
+
+
+with open("./input.txt") as file:
+    data = file.readlines()
+
+    first_part(data)
+    second_part(data)
+
