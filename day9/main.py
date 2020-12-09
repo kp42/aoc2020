9da88664c777374b7ebe6e87d9d890180bc3c2e3
Kaloyan Petrov <kaloyan.petrov7@gmail.com>
2020-12-09 10:38:54 +0200

    [day9][part2] Add python implementation

--- day9/main.py
+++ day9/main.py
@@ -6,7 +6,7 @@ def read_lines(filename):
         return [int(line.strip()) for line in file.readlines()]
 
 
-def first_part(lines, preamble_length):
+def get_invalid_number(lines, preamble_length):
     result = 0
     preamble_start = 0
     preamble = lines[preamble_start:preamble_length]
@@ -25,10 +25,37 @@ def first_part(lines, preamble_length):
     return result
 
 
+def get_contigious_set(lines, invalid_number):
+    # https://stackoverflow.com/a/46941568
+    result = []
+    for s, e in itertools.combinations(range(len(lines) + 1), 2):
+        if len(lines[s:e]) >= 2:
+            result.append(lines[s:e])
+
+    return result
+
+
+def first_part(lines, preamble_length):
+    return get_invalid_number(lines, preamble_length)
+
+
+def second_part(lines, preamble_length):
+    invalid_number = get_invalid_number(lines, preamble_length)
+    sets = get_contigious_set(lines, invalid_number)
+    result = 0
+
+    for s in sets:
+        if sum(s) == invalid_number:
+            result = min(s) + max(s)
+
+    return result
+
+
 def main():
     lines = read_lines("./input.txt")
 
     print(first_part(lines, 25))
+    print(second_part(lines, 25))
 
 
 if __name__ == "__main__":
